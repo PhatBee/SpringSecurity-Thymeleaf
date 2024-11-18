187e9222c97d43f72d061ac08afaec7ffe98b64e
PhatBee <vinhphatst1235@gmail.com>
2024-11-18 10:08:07 +0700

    Tao Entity

---
+++ src/main/java/vn/oneshop/springsecuritythymeleaf/entity/Products.java
@@ -0,0 +1,25 @@
+package vn.oneshop.springsecuritythymeleaf.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Products")
+public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "nvarchar(255)")
+    private String name;
+    @Column(columnDefinition = "nvarchar(255)")
+    private String brand;
+    @Column(columnDefinition = "nvarchar(255)")
+    private String madein;
+    private float price;
+
+}
---
+++ src/test/java/vn/oneshop/springsecuritythymeleaf/SpringSecurityThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.oneshop.springsecuritythymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
